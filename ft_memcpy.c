66a2bebdcd31b15c7f80bd6f1db843a6856d1acc
Mauro ABIDAL CARRER <maabidal@e2r8p2.clusters.42paris.fr>
2021-12-16 16:10:01 +0100

    oui

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 19:45:52 by maabidal          #+#    #+#             */
-/*   Updated: 2021/12/16 15:34:08 by maabidal         ###   ########.fr       */
+/*   Updated: 2021/12/16 15:58:11 by maabidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-void	*memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char	*i1;
 	unsigned char	*i2;
--- ft_memmove.c
+++ ft_memmove.c
@@ -6,13 +6,13 @@
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 19:46:26 by maabidal          #+#    #+#             */
-/*   Updated: 2021/12/16 15:52:30 by maabidal         ###   ########.fr       */
+/*   Updated: 2021/12/16 15:58:31 by maabidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"libft.h"
 
-void	*memcpy(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dest, const void *src, size_t n)
 {
 	unsigned int	i;
 	unsigned char	*d;
