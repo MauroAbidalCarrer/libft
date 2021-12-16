5fd837b920b9e78e8edbc9dda5f449f5a3afca82
Mauro ABIDAL CARRER <maabidal@e2r8p2.clusters.42paris.fr>
2021-12-16 15:20:07 +0100

    oui

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 19:45:52 by maabidal          #+#    #+#             */
-/*   Updated: 2021/12/13 19:46:07 by maabidal         ###   ########.fr       */
+/*   Updated: 2021/12/16 15:16:52 by maabidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,19 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	unsigned int	i;
-	unsigned char	*d;
-	unsigned char	*s;
+	unsigned char	*i1;
+	unsigned char	*i2;
 
-	if (!dst || !src)
-		return (NULL);
-	s = (unsigned char *)src;
-	d = (unsigned char *)dst;
-	if (s < d)
+	i1 = (unsigned char *)src;
+	i2 = (unsigned char *)dest;
+	if (!dest && !src)
+		return (dest);
+	while (n > 0)
 	{
-		while (n > 0)
-		{
-			n--;
-			d[n] = s[n];
-		}
-		return (d);
+		*i2 = *i1;
+		i1++;
+		i2++;
+		n--;
 	}
-	i = 0;
-	while (i < n)
-	{
-		d[i] = s[i];
-		i++;
-	}
-	return (d);
+	return (dest);
 }
--- ft_memmove.c
+++ ft_memmove.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   memove.c                                           :+:      :+:    :+:   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: maabidal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 19:46:26 by maabidal          #+#    #+#             */
-/*   Updated: 2021/12/13 19:46:40 by maabidal         ###   ########.fr       */
+/*   Updated: 2021/12/16 15:03:50 by maabidal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	return (ft_memcpy(dst, src, n));
+	unsigned int	i;
+	unsigned char	*d;
+	unsigned char	*s;
+
+	if (!dst || !src)
+		return (NULL);
+	s = (unsigned char *)src;
+	d = (unsigned char *)dst;
+	if (s < d)
+	{
+		while (n > 0)
+		{
+			n--;
+			d[n] = s[n];
+		}
+		return (d);
+	}
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (d);
 }
